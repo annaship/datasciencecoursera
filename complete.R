12be10f63954f22cea025cf9b9bb1c892b229428
annaship <tetya-loshad@yandex.ru>
2020-11-14 11:06:24 -0500

    Update complete.R
    correct names

--- complete.R
+++ complete.R
@@ -4,19 +4,8 @@ complete <- function(directory, id = 1:332) {
   files_full <- list.files(path = directory, pattern = "*.csv", full.names = T)
   tmp <- lapply(files_full[id], read.csv)
   dat <- do.call(rbind, tmp)
-  # mean(dat[, pollutant], na.rm = TRUE)
-  # length(!is.na(dat$sulfate) & !is.na(dat$nitrate))
-  # subset(andy$Weight, andy$Day==30)
-  # length(dat$ID[!is.na(dat$sulfate) & !is.na(dat$nitrate)])
-  # (tmp$ID[!is.na(tmp$sulfate) & !is.na(tmp$nitrate)])
-  # > for(f in tmp[1:2]) {
-  #     print(head(f))
-  #     f$ID[!is.na(f$sulfate) & !is.na(f$nitrate)]
-  # }
-  # t(as.data.frame(table(com))[,2]) 
   com <- dat$ID[!is.na(dat$sulfate) & !is.na(dat$nitrate)]
-  as.data.frame(table(com), col.names = c("id", "nobs"))
-  #names(com_res) <- c("id", "nobs")
-  
-  
+  com_res <- as.data.frame(table(com))
+  names(com_res) <- c("id", "nobs")
+  com_res
 }
