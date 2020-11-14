636f71f7f468667b6b8f0317d18ce4aafcd0850d
annaship <tetya-loshad@yandex.ru>
2020-11-13 22:26:25 -0500

    Update pollutantmean.R
    from help

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,29 +1,35 @@
 #  Write a function named 'pollutantmean' that calculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors. The function 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'. Given a vector monitor ID numbers, 'pollutantmean' reads that monitors' particulate matter data from the directory specified in the 'directory' argument and returns the mean of the pollutant across all of the monitors, ignoring any missing values coded as NA. A prototype of the function is as follows
 
-# directory <- "/Users/ashipunova/work/data_science_coursera/intro_r/specdata"
-# library(magrittr)
-# library(readr)
-
-# filenames <- list.files(directory, pattern="*.csv", full.names=TRUE)
-# ldf <- lapply(filenames, read.csv)
-# res <- lapply(ldf, summary)
-# head(ldf[[1]]["ID"])
-
-# useNA == "no"
-
-# > otm6 <- subset(mydata , mydata$Month == 6)
-# > mean(otm6$Temp)
-
-# x[!is.na(x) & x > 0]
-
-
-pollutantmean <- function(directory, pollutant, id = 1:332) {
+pollutantmean0 <- function(directory, pollutant, id = 1:332) {
   files <- list.files(path = directory, pattern = "*.csv", full.names = T)
-  all_means <- vector("list", length = length(id))
+  all_means <- vector("numeric", length = length(id))
   for (i in id) {
       df <- read.csv(files[i])
       mean.df <- mean(df[, pollutant], na.rm = TRUE)
-      all_means[[i]] <- mean.df
+      all_means[i] <- mean.df
   }
-  print(all_means)
+  mean(all_means, na.rm = TRUE)
 }
+
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  files_list <- list.files(path = directory, pattern = "*.csv", full.names = T)
+  dat <- data.frame()
+  
+  for (i in id) {
+    dat <- rbind(dat, read.csv(files_list[i]))
+  }
+  mean(dat[, pollutant], na.rm = TRUE)
+}
+
+weightmedian <- function(directory, day)  {
+  files_list <- list.files(directory, full.names=TRUE)   #creates a list of files
+  dat <- data.frame()                             #creates an empty data frame
+  for (i in 1:5) {                                
+    #loops through the files, rbinding them together 
+    dat <- rbind(dat, read.csv(files_list[i]))
+  }
+  dat_subset <- dat[which(dat[, "Day"] == day),]  #subsets the rows that match the 'day' argument
+  median(dat_subset[, "Weight"], na.rm=TRUE)      #identifies the median weight 
+  #while stripping out the NAs
+}
+
