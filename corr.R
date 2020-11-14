80c985deacd174931881073be5f61fcc2e44bcbd
annaship <tetya-loshad@yandex.ru>
2020-11-14 11:09:54 -0500

    Create corr.R

---
+++ corr.R
@@ -0,0 +1,11 @@
+# Write a function that reads a directory full of files and reports the number of completely observed cases in each data file. The function should return a data frame where the first column is the name of the file and the second column is the number of complete cases.
+
+complete <- function(directory, id = 1:332) {
+  files_full <- list.files(path = directory, pattern = "*.csv", full.names = T)
+  tmp <- lapply(files_full[id], read.csv)
+  dat <- do.call(rbind, tmp)
+  com <- dat$ID[!is.na(dat$sulfate) & !is.na(dat$nitrate)]
+  com_res <- as.data.frame(table(com))
+  names(com_res) <- c("id", "nobs")
+  com_res
+}
