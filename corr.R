05a2de0089d35e484a356bd210af0162eb198da8
annaship <tetya-loshad@yandex.ru>
2020-11-15 14:27:56 -0500

    Update corr.R
    works with try()

--- corr.R
+++ corr.R
@@ -1,6 +1,7 @@
 
 # Write a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
 com_result <- complete("specdata")
+is.error <- function(x) inherits(x, "try-error")
 
 corr <- function(directory, threshold = 0) {
   files_full <- list.files(path = directory, pattern = "*.csv", full.names = T)
@@ -15,28 +16,22 @@ corr <- function(directory, threshold = 0) {
 
   len <- length(selected_sulfate_list)
   all_cors <- vector("numeric", length = length(len))
+
   for (i in 1:len) {
-    tryCatch(
-      mt_sulf <- selected_sulfate_list[[i]],
-      error = function(e){
-        message("An error occurred in selected_sulfate_list[[i]]:\n", e)
-      },
-      #finally = mt_sulf <- numeric()
+    sulf_err <- is.error(try(selected_sulfate_list[[i]], silent = TRUE))
+    nitr_err <- is.error(try(selected_nitrate_list[[i]], silent = TRUE))
+    if(!sulf_err & !nitr_err) {
+      
+      mt_sulf <- selected_sulfate_list[[i]]
+      mt_nitr <- selected_nitrate_list[[i]]
+
+      all_cors[[i]] <- cor(mt_sulf, mt_nitr, use = "pairwise.complete.obs")
+      
+    }
+    else {
       return(numeric())
-    )
-    tryCatch(
-      mt_nitr <- selected_nitrate_list[[i]],
-      error = function(e){
-        message("An error occurred in selected_nitrate_list[[i]]:\n", e)
-      },
-      return(numeric())
-      #finally = mt_nitr <- numeric()
-        #all_cors[[i]] < numeric()
-    )
-    
-    
-    
-    all_cors[[i]] <- cor(mt_sulf, mt_nitr, use = "pairwise.complete.obs")
+    }
+
   }
   all_cors
 }
