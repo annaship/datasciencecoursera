85e55616d7ef13dbc290f5267c38dde2ee5c1659
annaship <tetya-loshad@yandex.ru>
2020-11-15 13:48:48 -0500

    Update corr.R
    works

--- corr.R
+++ corr.R
@@ -1,12 +1,33 @@
 
 # Write a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
+com_result <- complete("specdata")
 
 corr <- function(directory, threshold = 0) {
-  com_res0 <- com_res$id[as.numeric(as.character(com_res$nobs)) > threshold]
+  files_full <- list.files(path = directory, pattern = "*.csv", full.names = T)
+  tmp <- lapply(files_full, read.csv)
+  dat <- do.call(rbind, tmp)
+  
+  print("unique(dat$ID) 1")
+  print(unique(dat$ID))
+  com_res0 <- com_result$id[as.numeric(as.character(com_result$nobs)) > threshold]
+  print("unique(dat$ID) 2")
+  print(unique(dat$ID))
+  
   ok_ids <- as.numeric(as.character(com_res0))
-  selected_sulfate_list <- lapply(ok_ids, function(x){dat$sulfate[dat$ID == x]}) 
-  selected_nitrate_list <- lapply(ok_ids, function(x){dat$nitrate[dat$ID == x]})
+  print("ok_ids")
+  print(ok_ids)
 
+  print("unique(dat$ID) 3")
+  print(unique(dat$ID))
+  
+  selected_sulfate_list <- lapply(ok_ids, function(x){dat$sulfate[dat$ID == x]}) 
+  print("unique(dat$ID) 4")
+  print(unique(dat$ID))
+  
+  selected_nitrate_list <- lapply(ok_ids, function(x){dat$nitrate[dat$ID == x]})
+  print("unique(dat$ID) 5")
+  print(unique(dat$ID))
+  
   len <- length(selected_sulfate_list)
   all_cors <- vector("numeric", length = length(len))
   for (i in 1:len) {
