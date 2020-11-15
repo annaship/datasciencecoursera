8fcb34fff0bf8be6efbbb0d864d79aea3e987848
annaship <tetya-loshad@yandex.ru>
2020-11-14 19:58:25 -0500

    Update corr.R

--- corr.R
+++ corr.R
@@ -2,19 +2,16 @@
 # Write a function that takes a directory of data files and a threshold for complete cases and calculates the correlation between sulfate and nitrate for monitor locations where the number of completely observed cases (on all variables) is greater than the threshold. The function should return a vector of correlations for the monitors that meet the threshold requirement. If no monitors meet the threshold requirement, then the function should return a numeric vector of length 0. A prototype of this function follows
 
 corr <- function(directory, threshold = 0) {
-  # files_full <- list.files(path = directory, pattern = "*.csv", full.names = T)
-  # tmp <- lapply(files_full, read.csv)
-  # dat <- do.call(rbind, tmp)
-  # com <- dat$ID[!is.na(dat$sulfate) & !is.na(dat$nitrate)]
-  # com_res <- as.data.frame(table(com))
-  # names(com_res) <- c("id", "nobs")
   com_res0 <- com_res$id[as.numeric(as.character(com_res$nobs)) > threshold]
   ok_ids <- as.numeric(as.character(com_res0))
   selected_sulfate_list <- lapply(ok_ids, function(x){dat$sulfate[dat$ID == x]}) 
+  # head(str(selected_sulfate_list))
+  # List of 31
+  # $ : num [1:3652] NA NA NA NA NA NA NA NA NA NA ...
+  # $ : num [1:4018] NA NA NA NA NA NA NA NA NA NA ...
+  
+  selected_nitrate_list <- lapply(ok_ids, function(x){dat$nitrate[dat$ID == x]})
+ 
   selected_sulfate <- do.call(rbind, selected_sulfate_list)
-  
-  selected_nitrate_list <- lapply(ok_ids, function(x){dat$nitrate[dat$ID == x]}) 
-  selected_nitrate <- do.call(rbind, selected_nitrate_list)
-  
-   
+  selected_nitrate <- do.call(rbind, selected_nitrate_list)  
 }
