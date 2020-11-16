884f731464e2574aed6b4383a7a262ed392b64ec
annaship <tetya-loshad@yandex.ru>
2020-11-15 19:19:50 -0500

    Update complete.R
    sort

--- complete.R
+++ complete.R
@@ -1,5 +1,7 @@
 # Write a function that reads a directory full of files and reports the number of completely observed cases in each data file. The function should return a data frame where the first column is the name of the file and the second column is the number of complete cases.
 
+is.decr <- function(v){all(diff(v) <= 0)}
+
 complete <- function(directory, id = 1:332) {
   files_full <- list.files(path = directory, pattern = "*.csv", full.names = T)
   tmp <- lapply(files_full[id], read.csv)
@@ -7,5 +9,5 @@ complete <- function(directory, id = 1:332) {
   com <- dat$ID[!is.na(dat$sulfate) & !is.na(dat$nitrate)]
   com_res <- as.data.frame(table(com))
   names(com_res) <- c("id", "nobs")
-  com_res
+  com_res[order(com_res$id, decreasing = is.decr(id)),]
 }
