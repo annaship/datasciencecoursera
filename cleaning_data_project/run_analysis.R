fbd836e28936e5ba94b529a8e520fc7ed79fdaf5
annaship <tetya-loshad@yandex.ru>
2020-11-22 21:00:33 -0500

    Update run_analysis.R
    gzip csv

--- cleaning_data_project/run_analysis.R
+++ cleaning_data_project/run_analysis.R
@@ -83,4 +83,5 @@ activity_mean_std_set %>%
   group_by(subject, activity) %>%
   summarise(across(everything(), list(mean))) -> all_means
 
-
+out_file_name <- paste(file_destination, "/tidy_all_means.csv.gz", sep = "")
+write.csv(all_means, file=gzfile(out_file_name))
