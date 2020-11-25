a41be4c87c819c4387e77aa0cc2ece25175d60ff
annaship <tetya-loshad@yandex.ru>
2020-11-25 14:40:58 -0500

    Update plot_project.R
    to functions

--- plot_project.R
+++ plot_project.R
@@ -4,51 +4,71 @@ library(lubridate)
 library(stringi)
 library(data.table)
 
-# file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-# if(!file.exists("./data")){ dir.create("./data") }
-# temp <- tempfile()
-# download.file(file_url, temp)
-#
 file_destination <- "./data/"
 file_name <- paste(file_destination, "household_power_consumption.txt", sep = "")
-#
-# unzip(temp, exdir = file_destination)
-# unlink(temp)
-#
-data_file <- file(file_name, "r")
 
-used_dates <- c("2007-02-01", "2007-02-02") %>% 
-  ymd() %>%
-  format("%e/%m/%Y")
-single_digit_used_dates <- gsub("/0", "/", used_dates)
+download_data = function() {
+  file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  if(!file.exists("./data")){ dir.create("./data") }
+  temp <- tempfile()
+  download.file(file_url, temp)
 
-grep_expr_list <- lapply(single_digit_used_dates, function(d) {
-  paste("^", trimws(d[1]), ";", sep = "")
-})
-grep_expr <- str_c(grep_expr_list, sep = "", collapse = "|")
-
-# grep -e "^1/2/2007;\|^2/2/2007;" household_power_consumption.txt | tail
-
-lines <- list()
-i <- 1
-
-# Headers
-lines[[i]] <- readLines(data_file, 1)
-i <- i + 1
-
-while(TRUE) {
-  line = readLines(data_file, 1)
-  # browser()
-  if (length(line) == 0) break
-  else if(grepl(grep_expr, line)) {
-    lines[[i]] <- line
-    i <- i + 1
-  }
+  unzip(temp, exdir = file_destination)
+  unlink(temp)
 }
 
+make_grep_expression = function() {
+  used_dates <- c("2007-02-01", "2007-02-02") %>% 
+    ymd() %>%
+    format("%e/%m/%Y")
+  single_digit_used_dates <- gsub("/0", "/", used_dates)
+  
+  grep_expr_list <- lapply(single_digit_used_dates, function(d) {
+    paste("^", trimws(d[1]), ";", sep = "")
+  })
+  grep_expr <- str_c(grep_expr_list, sep = "", collapse = "|")
+  
+  # grep -e "^1/2/2007;\|^2/2/2007;" household_power_consumption.txt | tail
+  grep_expr
+}
+
+read_data = function() {
+  data_file <- file(file_name, "r")
+  on.exit(close(data_file))
+  
+  grep_expr <- make_grep_expression()
+  lines <- list()
+  i <- 1
+  
+  # Headers
+  lines[[i]] <- readLines(data_file, 1)
+  i <- i + 1
+  
+  while(TRUE) {
+    line = readLines(data_file, 1)
+    # browser()
+    if (length(line) == 0) break
+    else if(grepl(grep_expr, line)) {
+      lines[[i]] <- line
+      i <- i + 1
+    }
+  }
+  
+  lines
+}
+
+clean_data <- function(lines) {
 fread(text = paste(lines, collapse='\n')) %>%
   mutate(date_time = mdy_hms(paste(Date, Time)), .keep = "unused") %>% 
   relocate(date_time) %>%
   # str()
   {.} -> my_dataset
+  
+  my_dataset
+}
 
+# __main__
+
+lines <- read_data()
+my_dataset <- clean_data(lines)
+str(my_dataset)
