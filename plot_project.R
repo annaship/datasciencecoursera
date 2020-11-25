5de1421ca6dca927e5ee22c6eeaf4ab867345ee2
annaship <tetya-loshad@yandex.ru>
2020-11-25 13:41:56 -0500

    Update plot_project.R
    grep expr for dates

--- plot_project.R
+++ plot_project.R
@@ -1,22 +1,39 @@
-file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-if(!file.exists("./data")){ dir.create("./data") }
-temp <- tempfile()
-download.file(file_url, temp)
+library(stringr)
+library(dplyr)
+library(lubridate)
 
+# file_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+# if(!file.exists("./data")){ dir.create("./data") }
+# temp <- tempfile()
+# download.file(file_url, temp)
+#
 file_destination <- "./data/"
 file_name <- paste(file_destination, "household_power_consumption.txt", sep = "")
-
-unzip(temp, exdir = file_destination)
-unlink(temp)
-
+#
+# unzip(temp, exdir = file_destination)
+# unlink(temp)
+#
 data_file <- file(file_name, "r")
-good_dates <- c("2007-02-01", "2007-02-02")
+used_dates <- c("2007-02-01", "2007-02-02") %>% 
+  ymd() %>%
+  format("%e/%m/%Y")
+
+single_digit_used_dates <- gsub("/0", "/", used_dates)
+
+grep_expr_list <- lapply(single_digit_used_dates, function(d) {
+  paste("^", trimws(d[1]), ";", sep = "")
+})
+
+grep_expr <- str_c(grep_expr_list, sep = "", collapse = "|")
+
+# grep -e "^1/2/2007;\|^2/2/2007;" household_power_consumption.txt | tail
 
 lines <- c()
 while(TRUE) {
   line = readLines(data_file, 1)
-  if(length(line) == 0) break
-  else if(grepl("^2007-02-01;", line) | grepl("^2007-02-02;", line)) {
+  # browser()
+  if (length(line) == 0) break
+  else if(grepl(grep_expr, line)) {
     lines <- c(lines, line)
     }
 }
