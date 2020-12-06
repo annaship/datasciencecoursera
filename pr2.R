b56db53428b459d433a13dcc74215e6868094842
annaship <tetya-loshad@yandex.ru>
2020-12-06 18:08:50 -0500

    Update pr2.R
    works with functions

--- pr2.R
+++ pr2.R
@@ -1,90 +1,84 @@
 get_max10 = function(input_data, clmn_name) {
   input_data %>%
-  group_by(EVTYPE) %>%
-    arrange(-!!as.symbol(clmn_name)) %>%
-    slice(10)
+    slice_max(order_by = !!as.symbol(clmn_name), n = 10)
 }
 
-# !!as.symbol(column)
-# df %>% 
-#   filter_at(vars(column), any_vars(. == 1))
+fat_10_max <- get_max10(storm_data_csv, "FATALITIES")
 
+inj_10_max <- get_max10(storm_data_csv, "INJURIES")
 
-a10_1 <- get_max10(storm_data_csv, "FATALITIES")
+unique_max = function(input_data) {
+  input_data %>% 
+    aggregate(fat_inj ~ EVTYPE, ., max) %>%
+    left_join(input_data) 
+}
 
-storm_data_csv[order(-storm_data_csv$FATALITIES),] %>% 
-  select(FATALITIES) %>%
-  head(10) %>%
-  {.} -> fat10
+prepare_for_join = function(input_data, clmn_name) {
+  input_data %>% 
+    select(EVTYPE, fat_inj = !!as.symbol(clmn_name)) %>%
+    transform(health_type = clmn_name) %>%
+    group_by(EVTYPE)
+}
 
-storm_data_csv[order(-storm_data_csv$INJURIES),] %>% 
-  select(INJURIES) %>%
-  head(10) %>%
-  {.} -> inj10
+new_data_inj0 <- prepare_for_join(inj_10_max, "INJURIES")
+new_data_fat0 <- prepare_for_join(fat_10_max, "FATALITIES")
 
-storm_data_csv %>%
-  filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
-  select(STATE__, STATE, EVTYPE, FATALITIES, INJURIES) %>%
-  group_by(EVTYPE) %>%
-  {.} -> new_data_health
+# new_data_fat %>% aggregate(fat_inj ~ EVTYPE, ., max) %>%
+#   left_join(new_data_fat) %>%
+#   {.} -> new_data_fat
+# 
+new_data_fat <- unique_max(new_data_fat0)
+new_data_inj <- unique_max(new_data_inj0)
 
+new_data_health <- rbind(new_data_fat, new_data_inj)
 
-storm_data_csv %>%
-  filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
-  select(STATE__, STATE, EVTYPE, fat_inj = FATALITIES) %>%
-  transform(health_type = "FATALITIES") %>%
-  {.} -> new_data_fat
+# storm_data_csv[order(-storm_data_csv$FATALITIES),] %>% 
+#   select(FATALITIES) %>%
+#   head(10) %>%
+#   {.} -> fat10
+# 
+# storm_data_csv[order(-storm_data_csv$INJURIES),] %>% 
+#   select(INJURIES) %>%
+#   head(10) %>%
+#   {.} -> inj10
 
-new_data_fat %>% aggregate(fat_inj ~ EVTYPE, ., max) %>%
-    left_join(new_data_fat) %>%
-  {.} -> new_data_fat
-
+# storm_data_csv %>%
+#   filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
+#   select(STATE__, STATE, EVTYPE, FATALITIES, INJURIES) %>%
+#   group_by(EVTYPE) %>%
+#   {.} -> new_data_health
+# 
+# 
+# storm_data_csv %>%
+#   filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
+#   select(STATE__, STATE, EVTYPE, fat_inj = FATALITIES) %>%
+#   transform(health_type = "FATALITIES") %>%
+#   {.} -> new_data_fat
   
   # group_by(EVTYPE) %>%
 
-mydf %>%
-  group_by(Sample) %>% # for each unique sample
-  arrange(-total_reads) %>% # order by total_reads DESC
-  slice(1) # select the first row, i.e. with highest total_reads
+# mydf %>%
+#   group_by(Sample) %>% # for each unique sample
+#   arrange(-total_reads) %>% # order by total_reads DESC
+#   slice(1) # select the first row, i.e. with highest total_reads
+# 
+# aggregate(fat_inj ~ EVTYPE, new_data_fat, max) %>% merge()
+# merge(aggregate(fat_inj ~ EVTYPE, new_data_fat, max), new_data_fat)
 
-aggregate(fat_inj ~ EVTYPE, new_data_fat, max) %>% merge()
-merge(aggregate(fat_inj ~ EVTYPE, new_data_fat, max), new_data_fat)
 
+# storm_data_csv %>%
+#   filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
+#   select(STATE__, STATE, EVTYPE, fat_inj = INJURIES) %>%
+#   transform(health_type = "INJURIES") %>%
+#   group_by(EVTYPE) %>%
+#   {.} -> new_data_inj
+# 
+# new_data_health <- rbind(new_data_fat, new_data_inj)
+# new_data_health_un <- unique(new_data_health[,c('EVTYPE','fat_inj','health_type')])
 
-storm_data_csv %>%
-  filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
-  select(STATE__, STATE, EVTYPE, fat_inj = INJURIES) %>%
-  transform(health_type = "INJURIES") %>%
-  group_by(EVTYPE) %>%
-  {.} -> new_data_inj
-
-new_data_health <- rbind(new_data_fat, new_data_inj)
-new_data_health_un <- unique(new_data_health[,c('EVTYPE','fat_inj','health_type')])
-
-ggplot(data=new_data_health_un, aes(x=EVTYPE, y=fat_inj, fill = health_type)) +
+ggplot(data=new_data_health, aes(x=EVTYPE, y=fat_inj, fill = health_type)) +
   geom_bar(stat="identity")
-ggplot(data=new_data_health_un, aes(x=EVTYPE, y=fat_inj, fill = health_type)) +
+ggplot(data=new_data_health, aes(x=EVTYPE, y=fat_inj, fill = health_type)) +
   geom_bar(stat="identity", position=position_dodge()) +
   geom_text(aes(label=fat_inj), vjust=1.6, color="white",
             position = position_dodge(0.9), size=3.5)
-# TODO:
-# 3
-# EXCESSIVE HEAT
-# 0
-# INJURIES
-# 13
-# EXCESSIVE HEAT
-# 67
-# FATALITIES
-# 14
-# EXCESSIVE HEAT
-# 74
-# FATALITIES
-# 17
-# EXCESSIVE HEAT
-# 99
-# FATALITIES
-# 20
-# EXCESSIVE HEAT
-# 135
-# INJURIES
