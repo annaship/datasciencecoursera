7c43c2a0ed3d2397a4d7dc9353c90a6af12ee953
annaship <tetya-loshad@yandex.ru>
2020-12-06 13:09:47 -0500

    Update pr2.R
    barpplot. todo: unique

--- pr2.R
+++ pr2.R
@@ -19,19 +19,23 @@ storm_data_csv %>%
   filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
   select(STATE__, STATE, EVTYPE, fat_inj = FATALITIES) %>%
   transform(health_type = "FATALITIES") %>%
+  group_by(EVTYPE) %>%
   {.} -> new_data_fat
 
 storm_data_csv %>%
   filter(FATALITIES >= min(fat10) | INJURIES >= min(inj10) ) %>% 
   select(STATE__, STATE, EVTYPE, fat_inj = INJURIES) %>%
   transform(health_type = "INJURIES") %>%
+  group_by(EVTYPE) %>%
   {.} -> new_data_inj
 
-rbind(new_data_fat, new_data_inj) %>% head(2)
+new_data_health <- rbind(new_data_fat, new_data_inj)
+new_data_health_un <- unique(new_data_health[,c('EVTYPE','fat_inj','health_type')])
 
-
-ggplot(data=new_data_health, aes(x=EVTYPE, y=FATALITIES)) +
+ggplot(data=new_data_health_un, aes(x=EVTYPE, y=fat_inj, fill = health_type)) +
   geom_bar(stat="identity")
-# Use position=position_dodge()
-ggplot(data=new_data_health, aes(x=EVTYPE, y=INJURIES)) +
-  geom_bar(stat="identity", position=position_dodge())
+ggplot(data=new_data_health_un, aes(x=EVTYPE, y=fat_inj, fill = health_type)) +
+  geom_bar(stat="identity", position=position_dodge()) +
+  geom_text(aes(label=fat_inj), vjust=1.6, color="white",
+            position = position_dodge(0.9), size=3.5)
+  
